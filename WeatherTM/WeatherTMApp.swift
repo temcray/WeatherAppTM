8e64b3f31a76bcf498b8af3773d856fd46ba4d15
Tatiana Mccray <ejamison06@gmail.com>
2026-03-21 11:38:28 -0500

    Initial Commit

---
+++ WeatherTM/WeatherTMApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTMApp.swift
+//  WeatherTM
+//
+//  Created by Tatiana6mo on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
